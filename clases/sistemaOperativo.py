f7ff0283b1e565c9cec6b8def6cbf95ab5355cd1
YDMS-ASIAOF <septembre323@gmail.com>
2025-11-23 17:48:12 -0300

    Update sistemaOperativo.py

---
+++ clases/sistemaOperativo.py
@@ -0,0 +1,13 @@
+class SistemaOperativo():
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def compararContenido(cls, carpeta1, carpeta2):
+        pass
+
+    @classmethod
+    def eliminarContenido(cls, path):
+        pass
+
